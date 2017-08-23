2d8db606d8b1ca9d9e39387bf708e522391b9224
yumaokao <ymkq9h@gmail.com>
2017-08-23 14:07:22 +0800

    insta.py to use CrawlerBot(Bot), manually patch for issue #170

--- tests/insta.py
+++ tests/insta.py
@@ -18,39 +18,9 @@ def crawler(bot, user_id):
         if f not in read_userids:
             crawler(bot, f)
 
-
-class Crawler():
+class CrawlerBot(Bot):
     def __init__(self):
-        self.bot = Bot()
-        self.read_userids = []
-
-
-    def login(self, username='', password=''):
-        self.bot.login(username=username, password=password)
-
-    def crawler_from_username(self, username):
-        user_id = self.bot.get_userid_from_username(username)
-        self.crawler(user_id)
-
-    def crawler(self, user_id):
-        medias = self.bot.get_user_medias(user_id)
-        username = self.bot.get_username_from_userid(user_id)
-        followings = self.bot.get_user_following(user_id)
-        self.bot.logger.info('{} {} has {} medias {} followings'.format(username, user_id, len(medias), len(followings)))
-        self.download_photos(medias)
-
-    def download_photos(self, medias, path='photos/'):
-        broken_items = []
-        if len(medias) == 0:
-            self.bot.logger.info("Nothing to downloads.")
-            return broken_items
-        self.bot.logger.info("Going to download %d medias." % (len(medias)))
-        for media in tqdm(medias):
-            if not self.bot.download_photo(media, path):
-                # delay.error_delay(self)
-                broken_items = medias[medias.index(media):]
-                break
-        return broken_items
+        super().__init__()
 
 
 def main():
@@ -61,12 +31,11 @@ def main():
     # parser.add_argument('hashtags', type=str, nargs='+', help='hashtags')
     args = parser.parse_args()
 
-    bot = Crawler()
+    bot = CrawlerBot()
     bot.login(username=args.username, password=args.password)
 
-    bot.crawler_from_username('yumaokao')
-    # user_id = bot.get_userid_from_username('yumaokao')
-    # crawler(bot, user_id)
+    user_id = bot.get_userid_from_username('yumaokao')
+    crawler(bot, user_id)
 
     '''
     for hashtag in args.hashtags:
