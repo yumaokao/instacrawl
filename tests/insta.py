fc81393c4a658646d967093208c8f4c091fa717a
yumaokao <ymkq9h@gmail.com>
2017-08-23 14:34:57 +0800

    insta.py: to use CrawlerBot with endless recursive downloading

--- tests/insta.py
+++ tests/insta.py
@@ -4,23 +4,25 @@ from tqdm import tqdm
 from instabot import Bot
 
 
-read_userids = []
-
-
-def crawler(bot, user_id):
-    medias = bot.get_user_medias(user_id)
-    username = bot.get_username_from_userid(user_id)
-    followings = bot.get_user_following(user_id)
-    print('{} {} has {} medias {} followings'.format(username, user_id, len(medias), len(followings)))
-    read_userids.append(user_id)
-    # bot.download_photos(medias)
-    for f in followings:
-        if f not in read_userids:
-            crawler(bot, f)
-
 class CrawlerBot(Bot):
     def __init__(self):
         super().__init__()
+        self.read_userids = []
+
+    def crawler_from_username(self, username):
+        userid = self.get_userid_from_username(username)
+        self.crawler(userid)
+
+    def crawler(self, userid):
+        medias = self.get_user_medias(userid)
+        username = self.get_username_from_userid(userid)
+        followings = self.get_user_following(userid)
+        self.logger.info('{} {} has {} medias {} followings'.format(username, userid, len(medias), len(followings)))
+        self.read_userids.append(userid)
+        self.download_photos(medias)
+        for u in followings:
+            if u not in read_userids:
+                self.crawler(u)
 
 
 def main():
@@ -34,18 +36,7 @@ def main():
     bot = CrawlerBot()
     bot.login(username=args.username, password=args.password)
 
-    user_id = bot.get_userid_from_username('yumaokao')
-    crawler(bot, user_id)
-
-    '''
-    for hashtag in args.hashtags:
-        medias = bot.get_hashtag_medias(hashtag)
-        print(medias)
-    user_id = bot.get_userid_from_username('yumaokao')
-    followings = bot.get_user_following(user_id)
-    print(user_id)
-    print(followings)
-    '''
+    bot.crawler_from_username('yumaokao')
 
 
 if __name__ == "__main__":
